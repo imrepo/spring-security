66144b4f81365173f9592ea862f8d764e681086e
Xavier Padro <xpadro.sw@gmail.com>
2017-08-16 12:40:26 +0200

    Basic in-memory secured webapp

---
+++ v3.2/basic-inmemory/src/main/java/xpadro/spring/security/web/TestController.java
@@ -0,0 +1,20 @@
+package xpadro.spring.security.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/secure", method = RequestMethod.GET)
+    public @ResponseBody String secured() {
+        return "Hello user!";
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public @ResponseBody String admin() {
+        return "Hello admin!";
+    }
+}
