387dbb353e7498dec7fcec0afcbd22d3890700a0
Xavier Padro <xpadro.sw@gmail.com>
2018-01-02 13:29:20 +0100

    Removed unused imports

--- v3.2/rememberme-token/src/main/java/xpadro/spring/security/web/AppController.java
+++ v3.2/rememberme-token/src/main/java/xpadro/spring/security/web/AppController.java
@@ -5,9 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Controller
 public class AppController {
 
