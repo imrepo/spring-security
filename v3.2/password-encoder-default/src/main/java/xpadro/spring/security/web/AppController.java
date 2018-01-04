8c7c132be7f5990b15e59adfe0693bad1de368b5
Xavier Padro <xpadro.sw@gmail.com>
2018-01-04 19:39:07 +0100

    Renamed user mapping

--- v3.2/password-encoder-default/src/main/java/xpadro/spring/security/web/AppController.java
+++ v3.2/password-encoder-default/src/main/java/xpadro/spring/security/web/AppController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AppController {
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
     public @ResponseBody String secured() {
         return "Hello user!";
     }
